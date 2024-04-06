cf2b05adaf3e6819ef28e770666296d5a4b5c896
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-05 20:28:19 -0700

    added 3 endpoints  still need to add the even or odd

---
+++ Controllers/MultiEndpointPastProjects.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniChallenge_5_7_Enndpoints_MVC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MultiEndpointPastProjects : ControllerBase
+    {
+        
+        [HttpGet("/MadLibMadness")]
+        public string MadLibMadness(string Name, string Location, string VerbOne, string VerbTwo, string Adverb, string Noun, string Adjective)
+        {
+            string madLibText = $"Last year, this very dashing person named {Name}, had to {VerbOne} toward the highly recognizable park in {Location} and had to {Adverb} {VerbTwo} a {Adjective} {Noun} as fast as they could. Everyone became uneasy at how crazy the whole situation was.  {Name} was forever known as the {Adverb} {Noun} {VerbTwo}er.";
+            return madLibText;
+        }
+
+        [HttpGet("/ReverseItAll")]
+            public string ReverseItAll(string userInput)
+            {
+                if (string.IsNullOrEmpty(userInput))
+                    return "User input is necessary";
+
+                char[] changeToCharArray = userInput.ToCharArray();
+                Array.Reverse(changeToCharArray);
+                
+                string reversed = new string (changeToCharArray);
+
+                return "Your original input was:  " + userInput + "!\nYour input reversed is:  " + reversed + "!";
+            }
+
+        [HttpGet("/ReverseItNumbersOnly")]
+        public string ReverseItNumbersOnly(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "Input is required";
+
+            if (!input.All(char.IsDigit))
+                return "Error:  Input must be an integer";
+
+            char[] changeToCharArray = input.ToCharArray();
+            Array.Reverse(changeToCharArray);
+
+            string reversed = new string(changeToCharArray);
+
+            return "Your original input is:  " + input + " \nYour input reversed is:  " + reversed;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// adding build services for adding controllers
+builder.Services.AddControllers();
+
+
 var app = builder.Build();
+
+// adding app Map Controllers to use controllers
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +23,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
